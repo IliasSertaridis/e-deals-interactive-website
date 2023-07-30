f937a98d562234adbe7eaeb2dd302985074fd3f0
PeriKor19008 <66087558+PeriKor19008@users.noreply.github.com>
2023-07-31 00:37:15 +0300

    Add files via upload

--- e_dealsAPI/DbConfig.php
+++ e_dealsAPI/DbConfig.php
@@ -1,26 +1,33 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "e_deals1";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
 
-$sql = "select * from test1;";
-$result = $conn->query($sql);
+function new_query($sqlQuery){
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+  $dbname = "e_deals1";
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["t0"]. " - Name: " . $row["t1"]. " " . $row["t2"]. "<br>";
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
   }
-} else {
-  echo "0 results";
+
+  $sql = $sqlQuery;
+  $result = $conn->query($sql);
+
+  $conn->close();
+  return $result;
 }
-$conn->close();
+
+$result=new_query("select * from test1;");
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["t0"]. " - Name: " . $row["t1"]. " " . $row["t2"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
 ?>
