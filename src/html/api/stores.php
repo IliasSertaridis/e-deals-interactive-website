6de52ab435b29c3044ff0a137fb20d7683ddefc7
Greg Kapadoukas <gregkap12@gmail.com>
2023-08-13 17:08:00 +0300

    Fixed stores API queries again

--- src/html/api/stores.php
+++ src/html/api/stores.php
@@ -6,40 +6,37 @@ if (isset($type)) {
     {
         case "supermarket":
             $result = DBQuery("SELECT name, ST_x(coordinates) AS longtitude, ST_y(coordinates) AS latitude, store_type FROM store WHERE store_type = 'supermarket';");
-            echo json_encode($result, JSON_UNESCAPED_UNICODE);
             break;
         case "convenience":
             $result = DBQuery("SELECT name, ST_x(coordinates) AS longtitude, ST_y(coordinates) AS latitude, store_type FROM store WHERE store_type = 'convenience';");
-            echo json_encode($result, JSON_UNESCAPED_UNICODE);
             break;
         default:
-        $json = json_encode((object) null);
+        $json = json_encode((object) null, JSON_UNESCAPED_UNICODE);
         echo $json;
-        break;
+        exit;
     }
 }
 else {
     $result = DBQuery("SELECT name, ST_x(coordinates) AS longtitude, ST_y(coordinates) AS latitude, store_type FROM store;");
 
-    $storesarray = array(
-        'type' => 'FeatureCollection',
-        'features' => array()
-    );
-
-    foreach($result as $row) {
-        $store = array(
-            'type' => 'Feature',
-            'properties' => array(
-                'name' => $row['name'],
-                'shop' => $row['store_type']),
-            'geometry' => array(
-                'type' => 'Point',
-                'coordinates' => array(
-                    floatval($row['longtitude']),
-                    floatval($row['latitude'])))
-        );
-        array_push($storesarray['features'], $store);
-    }
-    echo json_encode($storesarray);
 }
+$storesarray = array(
+    'type' => 'FeatureCollection',
+    'features' => array()
+);
+foreach($result as $row) {
+    $store = array(
+        'type' => 'Feature',
+        'properties' => array(
+            'name' => $row['name'],
+            'shop' => $row['store_type']),
+        'geometry' => array(
+            'type' => 'Point',
+            'coordinates' => array(
+                floatval($row['longtitude']),
+                floatval($row['latitude'])))
+    );
+    array_push($storesarray['features'], $store);
+}
+echo json_encode($storesarray, JSON_UNESCAPED_UNICODE);
 ?>
